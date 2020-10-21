4714a79273b3fda1a477a4bad761ebbb42377a69
cityn <cityn@user.com>
2020-10-21 20:46:08 +0900

    email 수정

--- SAProject/src/com/project/email/SMTPAuthenticatior.java
+++ SAProject/src/com/project/email/SMTPAuthenticatior.java
@@ -6,7 +6,7 @@ import javax.mail.PasswordAuthentication;
 public class SMTPAuthenticatior extends Authenticator{ 
 	@Override 
 	protected PasswordAuthentication getPasswordAuthentication(){ 
-		return new PasswordAuthentication("joygajoy22@gmail.com","Maroon5321"); 
+		return new PasswordAuthentication("p2a2m@svk.jp","1234"); 
 	} 
 }
 
